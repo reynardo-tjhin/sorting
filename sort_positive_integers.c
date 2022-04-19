10bec39260887a8dd04a0f8c92e34099bc215a7c
Reynardo Tjhin <reychen26@gmail.com>
2022-04-19 15:17:25 +1000

    Finished heap sort

--- sort_positive_integers.c
+++ sort_positive_integers.c
@@ -17,6 +17,8 @@ extern void selection_sort(int* arr);
 extern void bubble_sort(int* arr);
 extern void insertion_sort(int* arr);
 extern void counting_sort(int* arr);
+extern void heap_sort(int* arr, int length);
+extern void merge_sort(int* arr, int length);
 
 int main() {
 
@@ -132,11 +134,26 @@ int main() {
     // find the time
     calculate_time_difference(ts_start, ts_end);
 
+    printf("\n");
+
+    // heap sort
+    printf("Heap Sort\n");
+    // copy the array
+    for (int i = 0; i < NUMBER_OF_ELEMENTS; i++) {
+        arr_copy[i] = arr[i];
+    }
+    // find the start time
+    timespec_get(&ts_start, TIME_UTC);
+    // call selection sort function
+    heap_sort(arr_copy, NUMBER_OF_ELEMENTS);
+    // find the end time
+    timespec_get(&ts_end, TIME_UTC);
+    // find the time
+    calculate_time_difference(ts_start, ts_end);
+
     // quick sort
 
     // merge-sort
-
-    // heap sort
 
     // free the malloc'ed array
     free(arr_copy);
@@ -228,7 +245,7 @@ void counting_sort(int* arr) {
     }
     int* counting_arr = (int *) calloc(largest_number, sizeof(int));
     if (counting_arr == NULL) {
-        printf("Error: Counting sort fails!\n");
+        printf("Error: Counting sort fails due to calloc not enough!\n");
         return;
     }
     // put each element into the counting array
@@ -246,4 +263,91 @@ void counting_sort(int* arr) {
         }
     }
     free(counting_arr);
+}
+
+void heap_sort(int* arr, int length) {
+
+    // create a temporary heap
+    int* heap = (int *) malloc(sizeof(int) * length);
+    if (heap == NULL) {
+        printf("Error: Heap sort fails due to malloc not enough!\n");
+        return;
+    }
+    // put each element into heap
+    for (int i = 0; i < length; i++) {
+        heap[i] = arr[i];
+        // upheap to restore the heap order
+        int child = i;
+        while (child != 0) {
+            int parent;
+            // index is even means it is a right child
+            if (child % 2 == 0) {
+                parent = (child - 2) / 2; // right child is at index 2i + 2 (where i is the index of parent)
+            }
+            // index is odd means it is a left child
+            else {
+                parent = (child - 1) / 2; // left child is at index 2i + 1 (where i is the index of parent)
+            }
+
+            // swap the parent and the child if the parent is greater than the child
+            if (heap[parent] > heap[child]) {
+                int temp = heap[child];
+                heap[child] = heap[parent];
+                heap[parent] = temp;
+            }
+            child = parent;
+        }
+    }
+
+    // "remove" each element and put them back to arr to sort
+    int end_index_of_heap = length - 1;
+    for (int i = 0; i < length; i++) {
+        
+        arr[i] = heap[0]; // smallest is always the at the root of the heap
+        
+        // remove the root
+        heap[0] = heap[end_index_of_heap];
+        end_index_of_heap -= 1;
+
+        // down heap
+        int parent = 0;
+        while ((end_index_of_heap != -1) && (parent < end_index_of_heap)) {
+
+            // get the indices of left child and right child
+            int left_child = 2 * parent + 1;
+            if (left_child > end_index_of_heap) {
+                left_child = -1;
+            }
+            int right_child = 2 * parent + 2;
+            if (right_child > end_index_of_heap) {
+                right_child = -1;
+            }
+
+            // if the index is the last one
+            if (left_child == -1 && right_child == -1) {
+                break;
+            }
+
+            // get the smaller from the two
+            int smaller_one = 0; // the index of the smaller value from right and left children
+            if (left_child == -1) { smaller_one = right_child; }
+            else if (right_child == -1) { smaller_one = left_child; }
+            else {
+                if (heap[right_child] > heap[left_child]) {
+                    smaller_one = left_child;
+                } else {
+                    smaller_one = right_child;
+                }
+            }
+            
+            // swap if parent is larger
+            if (heap[parent] > heap[smaller_one]) {
+                int temp = heap[smaller_one];
+                heap[smaller_one] = heap[parent];
+                heap[parent] = temp;
+            }
+            parent = smaller_one;
+        }
+    }
+    free(heap);
 }
